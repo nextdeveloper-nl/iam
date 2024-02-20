9f2843509808a1c977293c6717ff9d535171326b
Harun Baris Bulut <baris.bulut@plusclouds.com>
2024-02-20 23:31:17 +0300

    User service pagination fixed

--- src/Services/UsersService.php
+++ src/Services/UsersService.php
@@ -26,4 +26,71 @@ class UsersService extends AbstractUsersService
 {
 
     // EDIT AFTER HERE - WARNING: ABOVE THIS LINE MAY BE REGENERATED AND YOU MAY LOSE CODE
+
+
+
+    public static function get(UsersQueryFilter $filter = null, array $params = []) : Collection|LengthAwarePaginator {
+        $user = UserHelper::me();
+
+        //  If user not logged in then we dont list users
+        if(!$user) {
+            return new Collection();
+        }
+
+        $users = AccountUsers::filter($filter)
+            ->where('iam_account_id', UserHelper::currentAccount()->id)
+            ->paginate();
+
+        return $users;
+    }
+
+    public static function assignUserToRole(Users $user, IAuthorizationRole $role) : void
+    {
+        //  We need to check first if the role is created in the database
+        $role = RolesService::getRole($role);
+
+        RolesService::assignUserToRole($user, $role);
+    }
+
+    /**
+     * Registers the user just by email address
+     *
+     * @param $email
+     * @return Users
+     * @throws \Exception
+     */
+    public static function createWithEmail($email) : Users
+    {
+        $user = self::create([
+            'email' =>  $email
+        ]);
+
+        return $user;
+    }
+
+    /**
+     * Manipulating the function here
+     *
+     * @param array $data
+     * @return Users
+     * @throws \Exception
+     */
+    public static function create(array $data) : Users {
+        if(!array_key_exists('common_language_id', $data)) {
+            $lang = Languages::withoutGlobalScopes()->where('code', App::currentLocale())->first();
+
+            if($lang == null)
+                $lang = Languages::withoutGlobalScopes()->where('code', 'en')->first();
+
+            $data['common_language_id'] = $lang->id;
+        }
+
+        return parent::create($data);
+    }
+
+    public static function getByEmail($email, $createUser = false) : ?Users {
+        $user = Users::where('email', $email)->first();
+
+        return $user;
+    }
 }
