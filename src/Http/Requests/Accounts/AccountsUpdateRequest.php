c220be52aed731329cfef5158535a52384be8154
Harun Baris Bulut <baris.bulut@plusclouds.com>
2025-02-05 23:23:19 +0300

    Profile image url is added to request validators

--- src/Http/Requests/Accounts/AccountsUpdateRequest.php
+++ src/Http/Requests/Accounts/AccountsUpdateRequest.php
@@ -14,13 +14,14 @@ class AccountsUpdateRequest extends AbstractFormRequest
     {
         return [
             'name' => 'nullable|string',
-        'common_domain_id' => 'nullable|exists:common_domains,uuid|uuid',
-        'common_country_id' => 'nullable|exists:common_countries,uuid|uuid',
-        'phone_number' => 'nullable|string',
-        'description' => 'nullable|string',
-        'iam_account_type_id' => 'nullable|exists:iam_account_types,uuid|uuid',
-        'is_active' => 'boolean',
-        'tags' => '',
+            'common_domain_id' => 'nullable|exists:common_domains,uuid|uuid',
+            'common_country_id' => 'nullable|exists:common_countries,uuid|uuid',
+            'phone_number' => 'nullable|string',
+            'description' => 'nullable|string',
+            'profile_image_url' => 'string',
+            'iam_account_type_id' => 'nullable|exists:iam_account_types,uuid|uuid',
+            'is_active' => 'boolean',
+            'tags' => '',
         ];
     }
     // EDIT AFTER HERE - WARNING: ABOVE THIS LINE MAY BE REGENERATED AND YOU MAY LOSE CODE\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n
